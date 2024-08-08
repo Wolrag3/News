bf7266aaa0c87d542f93db09d042b20285ca1288
Ahmed Salman <83619642+Wolrag3@users.noreply.github.com>
2024-08-08 15:27:53 -0700

    Working Absolutely Fine

---
+++ News/Views/ArticleItem.cs
@@ -0,0 +1,15 @@
+namespace News.Views;
+
+public class ArticleItem : ContentView
+{
+    public ArticleItem()
+    {
+        Content = new VerticalStackLayout
+        {
+            Children = {
+                new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+                }
+            }
+        };
+    }
+}
